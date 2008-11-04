fcbd88378caa28582bdfd8c9452bf2d10bb8b069
Adam Keys <adam@therealadam.com>
2008-11-03 21:31:22 -0600

    Return the jQuery object from bulletGraph(), move the example code into its own file.

--- bullet_graph.js
+++ bullet_graph.js
@@ -8,9 +8,7 @@
     
     return canvas.getContext("2d");
   };
-}(jQuery));
-
-(function($) {
+  
   $.fn.bulletGraph = function(min, max, marker, options) {
     options = $.extend({
       baseColor: '#444',
@@ -71,29 +69,7 @@
       drawRange(ctx);
       drawMarker(ctx);
     });
+    
+    return this;
   };
 }(jQuery));
-
-jQuery(function($) {
-  
-  $('.graph').bulletGraph(25, 75, 50, {height: 36});
-  $('.hugeGraph').bulletGraph(100, 300, 200, {width: 400, height: 108});
-  $('.neater').bulletGraph(150, 375, 350, {width: 360, drawEdges: false});
-  $('.posh').each(function() {
-    var el = $(this);
-    el.bulletGraph(el.children('.graph-low').text(),
-                   el.children('.graph-high').text(),
-                   el.children('.graph-marker').text(),
-                   {width: el.children('.graph-max').text()}, {height: 36});
-    el.children().empty('span');
-  });
-  $('.sparkline').each(function() {
-    var el = $(this);
-    el.bulletGraph(el.children('.graph-low').text(),
-                   el.children('.graph-high').text(),
-                   el.children('.graph-marker').text(),
-                   {width: el.children('.graph-max').text(), height: 10});
-    el.children().empty();
-  });
-  
-});
